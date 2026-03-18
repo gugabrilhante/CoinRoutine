724c73de492e87e8af2fc82a0b5911c497ca2303
gustavobrilhante <gugabrilhante@gmail.com>
2026-03-18 10:45:03 -0300

    Add NavController setup

--- composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/App.kt
+++ composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/App.kt
@@ -1,15 +1,70 @@
 package com.gustavo.brilhante.coinroutine
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.gustavo.brilhante.coinroutine.coins.presentation.CoinsListScreen
+import com.gustavo.brilhante.coinroutine.core.navigation.Buy
+import com.gustavo.brilhante.coinroutine.core.navigation.Coins
+import com.gustavo.brilhante.coinroutine.core.navigation.Portfolio
+import com.gustavo.brilhante.coinroutine.core.navigation.Sell
+import com.gustavo.brilhante.coinroutine.portfolio.presentation.PortfolioScreen
 import com.gustavo.brilhante.coinroutine.theme.CoinRoutineTheme
+import com.gustavo.brilhante.coinroutine.trade.presentation.buy.BuyScreen
+import com.gustavo.brilhante.coinroutine.trade.presentation.buy.SellScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    val navController = rememberNavController()
     CoinRoutineTheme {
+        NavHost(
+            navController = navController,
+            modifier = Modifier.fillMaxSize(),
+            startDestination = Portfolio
+        ){
+            composable<Portfolio> {
+                PortfolioScreen(
+                    onCoinItemClicked = { coinId -> // TODO: will be used later
+                        navController.navigate(Sell)
+                    },
+                    onDiscoverCoinsClicked = {
+                        navController.navigate(Coins)
+                    }
+                )
+            }
 
-        CoinsListScreen {  }
+            composable<Coins> {
+                CoinsListScreen { coinId -> // TODO: will be used later
+                    navController.navigate(Buy)
+                }
+            }
+
+            composable<Buy> { navBackStackEntry ->
+                BuyScreen(
+                    coinId = "todo",
+                    navigateToPortfolio = {
+                        navController.navigate(Portfolio) {
+                            popUpTo(Portfolio) { inclusive = true }
+                        }
+                    }
+                )
+            }
+
+            composable<Sell> { navBackStackEntry ->
+                SellScreen(
+                    coinId = "todo",
+                    navigateToPortfolio = {
+                        navController.navigate(Portfolio) {
+                            popUpTo(Portfolio) { inclusive = true }
+                        }
+                    }
+                )
+            }
+        }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/core/navigation/CoinRoutineScreen.kt
@@ -0,0 +1,15 @@
+package com.gustavo.brilhante.coinroutine.core.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Portfolio
+
+@Serializable
+object Coins
+
+@Serializable
+object Buy
+
+@Serializable
+object Sell
